d9bfc973786ff9317d04e9fde03ebc70d25139b4
kaahRomao <kaueromao1989@gamil.com>
2026-06-02 19:28:20 -0300

    +++

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
+//
 const elemento = {
   aside: document.querySelector("aside"),
   sections: document.querySelectorAll("section"),
@@ -26,60 +28,127 @@ function displayControler(id) {
     }
   });
 }
+//! Conversor de moedas --------------------------------------------------------------------------------------------
 
-//Calculadora de IMC
+async function getDados(url) {
+  let dadosApi = await fetch(url);
+  console.log(dadosApi);
+
+  let dados = await dadosApi.json();
+
+  console.log(dados);
+}
+getDados(url);
+//! Final Conversor de moedas -------------------------------------------------------------------------------------------
+
+//!Calculadora de IMC --------------------------------------------------------------------------------------------
 function calcularImc() {
+  // Seleciona os elementos de peso, altura e gênero
   let peso = document.querySelector("#peso");
   let altura = document.querySelector("#altura");
+  let genero = document.querySelector("#genero");
 
+  // Trativa de erros para os inputs de peso e altura
   if (altura.value === "0") {
-    document.querySelector(".resultado").textContent =
+    document.querySelector(".resultado-imc").textContent =
       "valor do Input Inválido";
     setTimeout(() => {
       peso.value = "";
       altura.value = "";
-      document.querySelector(".resultado").textContent = "";
+      document.querySelector(".resultado-imc").textContent = "";
     }, 3000);
     return;
   }
 
   if (peso.value === "" || altura.value == "") {
-    document.querySelector(".resultado").textContent = "Digite algum valor";
+    document.querySelector(".resultado-imc").textContent = "Digite algum valor";
     setTimeout(() => {
       peso.value = "";
       altura.value = "";
-      document.querySelector(".resultado").textContent = "";
+      document.querySelector(".resultado-imc").textContent = "";
     }, 3000);
     return;
   }
 
   if (isNaN(peso.value) || isNaN(altura.value)) {
-    document.querySelector(".resultado").textContent = "Digite um valor válido";
+    document.querySelector(".resultado-imc").textContent =
+      "Digite um valor válido";
     setTimeout(() => {
       peso.value = "";
       altura.value = "";
-      document.querySelector(".resultado").textContent = "";
+      document.querySelector(".resultado-imc").textContent = "";
     }, 3000);
     return;
   }
 
   if (Number(peso.value) < 0 || Number(altura.value) < 0) {
-    document.querySelector(".resultado").textContent = "Digite um valor válido";
+    document.querySelector(".resultado-imc").textContent =
+      "Digite um valor válido";
     setTimeout(() => {
       peso.value = "";
       altura.value = "";
-      document.querySelector(".resultado").textContent = "";
+      document.querySelector(".resultado-imc").textContent = "";
     }, 3000);
     return;
   }
-
+  // Calcula o IMC usando a fórmula: peso / (altura * altura)
   let resultado = peso.value / altura.value ** 2;
 
-  document.querySelector(".resultado").textContent = resultado.toFixed(2);
+  // Exibe o resultado do IMC com duas casas decimais
+  document.querySelector(".resultado-imc").textContent =
+    `${resultado.toFixed(2)} (${genero.value})`;
+
+  if (resultado < 18.5) {
+    document.querySelector(".grupos").textContent =
+      "Classificação: Abaixo do peso";
+    setTimeout(() => {
+      peso.value = "";
+      altura.value = "";
+      document.querySelector(".grupos").textContent = "";
+    }, 5000);
+  } else if (resultado <= 24.9) {
+    document.querySelector(".grupos").textContent = "Classificação: normal";
+    setTimeout(() => {
+      peso.value = "";
+      altura.value = "";
+      document.querySelector(".grupos").textContent = "";
+    }, 5000);
+  } else if (resultado <= 29.9) {
+    document.querySelector(".grupos").textContent = "Classificação: Sobrepeso";
+    setTimeout(() => {
+      peso.value = "";
+      altura.value = "";
+      document.querySelector(".grupos").textContent = "";
+    }, 5000);
+  } else if (resultado <= 39.9) {
+    document.querySelector(".grupos").textContent = "Classificação: Obesidade";
+    setTimeout(() => {
+      peso.value = "";
+      altura.value = "";
+      document.querySelector(".grupos").textContent = "";
+    }, 5000);
+  } else if (resultado >= 40) {
+    document.querySelector(".grupos").textContent =
+      "Classificação: Obesidade grave";
+    setTimeout(() => {
+      peso.value = "";
+      altura.value = "";
+      document.querySelector(".grupos").textContent = "";
+    }, 5000);
+  } else {
+    document.querySelector(".grupos").textContent =
+      "Não se encaixa em nenhum grupo!";
+    setTimeout(() => {
+      peso.value = "";
+      altura.value = "";
+      document.querySelector(".grupos").textContent = "";
+    }, 5000);
+  }
 
   setTimeout(() => {
     peso.value = "";
     altura.value = "";
-    document.querySelector(".resultado").textContent = "";
+    document.querySelector(".resultado-imc").textContent = "";
   }, 5000);
 }
+//! Final Calculadora de IMC -------------------------------------------------------------------------------------------
